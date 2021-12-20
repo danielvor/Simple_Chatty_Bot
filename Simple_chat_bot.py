5ee5eec805ae939f369fc207eb295333690d8e05
Daniel Oliveira <this.daniel@outlook.com>
2021-12-20 10:12:02 -0300

    Update Simple_chat_bot.py
    Tetando adivinhar sua idade usando operadores aritiméticos

--- Simple_chat_bot.py
+++ Simple_chat_bot.py
@@ -1,3 +1,5 @@
+# sending greatings
+
 print('Greetings My Friend! My name is Guinti.')
 print('I was created in 2021.')
 print('Please, remind me your name.')
@@ -6,3 +8,14 @@ your_name = input()
 # reading a name
 
 print('What a beautiful name! Nice to meet you, {}'.format(your_name))
+
+# asking remaiders of age
+
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+# reading all remainders and printing
+
+value = [int(input()) for i in range(3)]
+your_age = (value[0] * 70 + value[1] * 21 + value[2] * 15) % 105
+print("Your age is {}; that's a good time to start programming!".format(your_age))
