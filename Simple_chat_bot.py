7e9eaf28dbd2077c59c058248be61fe3d0c8cbd2
Daniel Oliveira <this.daniel@outlook.com>
2021-12-19 22:14:09 -0300

    Greetings
    Nesta fase, você se apresentará ao bot para que ele possa cumprimentá-lo pelo seu nome.

---
+++ Simple_chat_bot.py
@@ -0,0 +1,8 @@
+print('Greetings My Friend! My name is Guinti.')
+print('I was created in 2021.')
+print('Please, remind me your name.')
+your_name = input()
+
+# reading a name
+
+print('What a beautiful name! Nice to meet you, {}'.format(your_name))
