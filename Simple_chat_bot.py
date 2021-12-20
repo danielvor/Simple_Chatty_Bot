1b6d5184c0b1adadf7aef1ece9683d3adc3c05a3
Daniel Oliveira <this.daniel@outlook.com>
2021-12-20 10:38:37 -0300

    Update Simple_chat_bot.py
    Lendo um número e contando

--- Simple_chat_bot.py
+++ Simple_chat_bot.py
@@ -19,3 +19,14 @@ print('Enter remainders of dividing your age by 3, 5 and 7.')
 value = [int(input()) for i in range(3)]
 your_age = (value[0] * 70 + value[1] * 21 + value[2] * 15) % 105
 print("Your age is {}; that's a good time to start programming!".format(your_age))
+print('Now I will prove to you that I can count to any number you want.')
+
+# read a number and count to it here
+
+count = 0
+value = int(input())
+for i in range(value+1):
+    print('{} !'.format(count))
+    count+=1
+
+print('Completed, have a nice day!')
