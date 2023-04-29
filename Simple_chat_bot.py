b3e84b994f114798c5222a8c1aa0c695563e9269
Daniel Rodrigues <this.daniel@outlook.com>
2023-04-29 13:05:43 -0300

    Update Simple_chat_bot.py

--- Simple_chat_bot.py
+++ Simple_chat_bot.py
@@ -1,57 +1,62 @@
+# import sleep function from time module
+from time import sleep
+
+
+# Intro to the program
 def greet(bot_name, birth_year):
-    print('Hello! My name is ' + bot_name + '.')
+    print('Hi, there! My name is ' + bot_name + '...')
+    sleep(2) # wait 2 seconds
     print('I was created in ' + birth_year + '.')
+    sleep(2) # wait 2 seconds
 
-
+# Greets the user and asks for their name
 def remind_name():
-    print('Please, remind me your name.')
+    print('Please, remind me your name.')    
     name = input()
-    print('What a great name you have, ' + name + '!')
+    print('What a beautiful name you have, ' + name + ' !!!')
+    sleep(2) # wait 2 seconds
 
-
-def guess_age():
-    print('Let me guess your age.')
-    print('Enter remainders of dividing your age by 3, 5 and 7.')
-
-    rem3 = int(input())
-    rem5 = int(input())
-    rem7 = int(input())
-    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
-
-    print("Your age is " + str(age) + "; that's a good time to start programming!")
-
-
+# Counts to any number the user wants
 def count():
-    print('Now I will prove to you that I can count to any number you want.')
-
+    print('Like a robot I can count to any number you want.')
+    sleep(2) # wait 2 seconds
+    print('Please, tell me a number.')
     num = int(input())
-    curr = 0
-    while curr <= num:
-        print(curr, '!')
-        curr = curr + 1
+    current = 0
+    while current <= num:
+        if current < num:
+            print(current, ',', end=' ')
+        else :
+            print(current, end=" ! \n")
+        current += 1
+    sleep(2) # wait 2 seconds
 
-
-def test():
-    print("Let's test your programming knowledge.")
-    questions = ['Why do we use methods?', '1. To repeat a statement multiple times.', '2. To decompose a program into several small subroutines.',
-                '3. To determine the execution time of a program.', '4. To interrupt the execution of a program.']
+# Asks the user who create Guinti
+def juliana_leal():
+    # Quizz
+    print("Quick quizz.")
+    sleep(2) # wait 2 seconds
+    print('Who create Guinti?')
+    sleep(2) # wait 2 seconds
+    questions = ['alternatives:', '1. Juliana Leal', '2. Batman',
+                '3. Leonardo Ferreira', '4. Daniel Rodrigues']
     
     for q in questions:
-        print(q)
+        if q == questions[-1]:
+            print(q)
+        else:
+            print(q, end=" ")
     
-    while input() != '2':
+    while input() != '1':
         print('Please, try again.')
-
-    print('Completed, have a nice day!')
-
+    print('Correct, Guinte was create by' + questions[1][2:] + '!')
+    sleep(2) # wait 2 seconds
 
 def end():
     print('Congratulations, have a nice day!')
 
-
-greet('Aid', '2020')  # change it as you need
+greet('Guinti', '2020')
 remind_name()
-guess_age()
 count()
-test()
+juliana_leal()
 end()
